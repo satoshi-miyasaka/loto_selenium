5a331d6c19e20fbacd59bec3dea90a2ac93e989a
satoshi-miyasaka <satoshi.miyasaka.1000@gmail.com>
2023-06-26 01:38:38 +0900

    データインサートをPandasに変更

--- libs/create_table.py
+++ libs/create_table.py
@@ -3,43 +3,35 @@ import yaml
 import pandas
 
 def create_database(config):
-    conn = sqlite3.connect(config['common']['database'])
-    cur = conn.cursor()
-    return conn, cur
+    return sqlite3.connect(config['common']['database'])
 
-def create_table(cur, config):
+def create_table(conn, config):
     primary_key = config['primary_key']
     table = config['table']
     columns = ','.join([x + ' INTEGER' for x in config['columns'].keys()])
 
     sql = f'CREATE TABLE {table} ({columns}, PRIMARY KEY({primary_key}))'
-    cur.execute(sql)
+    conn.execute(sql)
 
-def insert_data(cur, config):
-    table = config['table']
-    questions = ','.join(['?' for x in range(len(config['columns'].keys()))])
+def insert_data(conn, config):
+    columns=[name for name in config['columns'].keys()]
+    df = pandas.read_csv(config['csv'], header=None)
+    df2 = df.set_axis(columns, axis='columns')
+    df2.to_sql(config['table'], con=conn, if_exists='append', index=False)
 
-    df = pandas.read_csv(config['csv'], header=None, index_col=0)
-    insert = f'INSERT INTO {table} VALUES({questions})'
-
-    for row in df.itertuples(name=None):
-        cur.execute(insert, row)
-
-    conn.commit()
-
-def close():
+def close(conn):
     conn.close()
 
 with open('config.yml', 'r') as yml:
     config = yaml.safe_load(yml)
 
-conn, cur = create_database(config)
+conn = create_database(config)
 
-create_table(cur, config['loto6'])
-insert_data(cur, config['loto6'])
+create_table(conn, config['loto6'])
+insert_data(conn, config['loto6'])
 
-create_table(cur, config['loto7'])
-insert_data(cur, config['loto7'])
+create_table(conn, config['loto7'])
+insert_data(conn, config['loto7'])
 
-close()
+close(conn)
 
